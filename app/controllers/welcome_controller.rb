a2f758b6d0e73cd67918ba2aae35bb71e899d4a4
Milind Mevada <milindmevada@gmail.com>
2018-03-06 19:56:28 +0530

    initial commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,20 @@
+class WelcomeController < ApplicationController
+  def index
+    result = {}
+    result =
+        case  params[:name]
+        when "hello"
+            {status: 200, value: "world"}
+        when "iorem"
+            {status: 404, value: "no_value"}
+        when "foobar"
+            {status: 500, value: "internal_error"}
+        when "ipsum"
+            {status: 403, value: "unauthorized"}
+        else
+            {status: 422, value: "bad_value"}
+        end
+    render status: result[:status], json: { data: result[:value] }.to_json
+end
+
+end
